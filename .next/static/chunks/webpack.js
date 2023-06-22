0e7de1167bce47da4f49478f6b95b479d0204911
noorcod <noorultracodes@gmail.com>
2023-06-22 11:28:58 +0500

    changes project from js to ts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58848640a5c9c148"; }
+/******/ 		__webpack_require__.h = function() { return "72f2dc738b8dc5d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/specs.js
+++ src/pages/specs.js
@@ -6,7 +6,7 @@ import Listing from '../sections/Specs/Listing'
 import React, { useState } from 'react'
 import { Breadcrumb, Col, Row } from 'react-bootstrap'
 import styles from "../styles/Specs.module.css"
-const specs = () => {
+const Specs = () => {
     const [mobileFilter, setMobileFilter] = useState();
 
     return (
@@ -38,4 +38,4 @@ const specs = () => {
     )
 }
 
-export default specs
+export default Specs
