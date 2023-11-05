96c0b1c0ff1b97e99b42fa991e3fc76acece6910
noorcod <noorultracodes@gmail.com>
2023-11-05 16:13:59 +0500

    backup

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
       'bufferutil': 'commonjs bufferutil',
+      'canvas':{}
     })
     return config
   },
-  canvas:{},
   development: {
     reactStrictMode: true,
   images: {
