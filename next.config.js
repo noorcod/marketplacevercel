278871d8e700e790f29db7eafe5d3170e6a4b061
noorcod <noorultracodes@gmail.com>
2023-11-05 16:02:16 +0500

    backup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const env = process.env.ENV || 'development';
 
 const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
   development: {
     reactStrictMode: true,
   images: {
